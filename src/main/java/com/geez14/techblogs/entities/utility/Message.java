4c4f0929f3ba1ace24b523380dbf3ffbf297069b
geez14 <imonabbar@noreply.com>
2025-01-08 06:58:50 +0530

    message entitiy is object to represend server response in bootstrap manner"

--- src/main/java/com/geez14/techblogs/entities/utility/Message.java
+++ src/main/java/com/geez14/techblogs/entities/utility/Message.java
@@ -1,4 +1,42 @@
 package com.geez14.techblogs.entities.utility;
 
 public class Message {
+    private String content;
+    private String type;
+    private String cssClass;
+
+    public Message() {
+        content = "empty message";
+        type="";
+        cssClass="";
+    }
+    public Message(String content, String type, String cssClass) {
+        this.content = content;
+        this.type = type;
+        this.cssClass = cssClass;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getCssClass() {
+        return cssClass;
+    }
+
+    public void setCssClass(String cssClass) {
+        this.cssClass = cssClass;
+    }
 }
