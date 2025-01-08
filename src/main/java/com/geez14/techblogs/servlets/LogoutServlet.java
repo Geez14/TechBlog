751784facc29f98fa15827d73a6ce7587ecf899a
geez14 <imonabbar@noreply.com>
2025-01-08 07:02:36 +0530

    implemented: Logout module

--- src/main/java/com/geez14/techblogs/servlets/LogoutServlet.java
+++ src/main/java/com/geez14/techblogs/servlets/LogoutServlet.java
@@ -1,4 +1,19 @@
 package com.geez14.techblogs.servlets;
 
-public class LogoutServlet {
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = "/logout")
+public class LogoutServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) {
+        request.getSession().invalidate();
+        // invalidate session and remove browser cache
+        response.setHeader("Cache-Control", "no-cache");
+        response.setHeader("Pragma", "no-cache");
+        response.setDateHeader("Expires", 0);
+    }
 }
